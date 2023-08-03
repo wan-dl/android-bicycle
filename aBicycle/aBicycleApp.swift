e6d0e5a643cf8144b712a1a9c6ceb9e1d8be4542
xiaohutu <shanxi.1991@gmail.com>
2023-08-03 22:49:18 +0800

    Initial Commit

---
+++ aBicycle/aBicycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  aBicycleApp.swift
+//  aBicycle
+//
+//  Created by 1 on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct aBicycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
